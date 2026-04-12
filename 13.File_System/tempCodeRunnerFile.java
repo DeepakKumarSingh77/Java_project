8647f1818f677b87e4fc1275af9361e20ab8b0ad
Deepak Kumar Singh <rishusingh13615@gmail.com>
2026-04-12 15:05:41 +0530

    adding File System

--- 13.File_System/tempCodeRunnerFile.java
+++
@@ -1,6 +0,0 @@
- FileSystemItem cwd = root.cd("docs");
-        if (cwd != null) {
-            cwd.ls(0);
-        } else {
-            System.out.println("\nCould not cd into docs\n");
-        }
